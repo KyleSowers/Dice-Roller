8921026a76480be50859f8abeafb53c783339f57
Kyle Sowers <kyle.a.sowers@gmail.com>
2025-07-13 00:05:18 -0400

    Showing a list & responding to dialog selections.

--- app/src/main/java/com/zybooks/diceroller/MainActivity.java
+++ app/src/main/java/com/zybooks/diceroller/MainActivity.java
@@ -3,6 +3,7 @@ package com.zybooks.diceroller;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.ParcelUuid;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,7 +13,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RollLengthDialogFragment.OnRollLengthSelectedListener {
 
     public static final int MAX_DICE = 3;
     public int mVisibleDice;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView[] mDiceImageViews;
     private Menu mMenu;
     private CountDownTimer mTimer;
+    private long mTimerLength = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +64,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onRollLengthClick(int which) {
+        // Convert to milliseconds
+        mTimerLength = 1000L * (which + 1);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
         // Determine which menu option was chosen
@@ -83,6 +91,10 @@ public class MainActivity extends AppCompatActivity {
             return true;
         } else if (item.getItemId() == R.id.action_roll) {
             rollDice();
+            return true;
+        } else if (item.getItemId() == R.id.action_roll_length) {
+            RollLengthDialogFragment dialog = new RollLengthDialogFragment();
+            dialog.show(getSupportFragmentManager(), "rollLengthDialog");
             return true;
         }
 
@@ -110,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
             mTimer.cancel();
         }
 
-        mTimer = new CountDownTimer(2000, 100) {
+        mTimer = new CountDownTimer(mTimerLength, 100) {
             public  void onTick(long millisUnitFinished) {
                 for (int i = 0; i < mVisibleDice; i++) {
                     mDice[i].roll();
